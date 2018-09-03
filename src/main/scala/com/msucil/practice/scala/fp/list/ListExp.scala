60472e36f4d5b122f2532d0e4d3e00c401fdc0bb
msucil <np.msushil@gmail.com>
2018-09-03 23:33:57 +0545

    Add concat, reverse, removeAt for List functions

--- src/main/scala/com/msucil/practice/scala/fp/list/ListExp.scala
+++ src/main/scala/com/msucil/practice/scala/fp/list/ListExp.scala
@@ -1,5 +1,8 @@
 package com.msucil.practice.scala.fp.list
 
+/**
+  * List fundamental function implementation
+  */
 class ListExp {
 
   // return last element of the list
@@ -16,4 +19,19 @@ class ListExp {
     case y :: ys => y :: init(ys)
   }
 
+  //Concatenation of List
+  // xs ::: ys ==> ys . ::: (xs)
+  def concat[T](xs: List[T], ys: List[T]): List[T] = {
+    xs match {
+      case List() => ys
+      case z :: zs => z :: concat(zs, ys) // complexity length of xs
+    }
+  }
+
+  def reverse[T](xs: List[T]): List[T] = xs match {
+    case List() => List()
+    case y :: ys => reverse(ys) ++ List(y) // complexity m * m where m is size
+  }
+
+  def removeAt[T](i: Int, xs: List[T]): List[T] = (xs take i) ::: (xs drop i + 1)
 }
