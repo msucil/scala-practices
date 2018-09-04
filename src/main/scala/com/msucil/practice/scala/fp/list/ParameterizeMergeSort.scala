4cf30c67b2ac8c209696f6444bb19ebb17e3e290
msucil <np.msushil@gmail.com>
2018-09-04 21:44:35 +0545

    Add Generic Merge Sort

---
+++ src/main/scala/com/msucil/practice/scala/fp/list/ParameterizeMergeSort.scala
@@ -0,0 +1,30 @@
+package com.msucil.practice.scala.fp.list
+
+object ParameterizeMergeSort {
+  def sort[T](xs: List[T])(lt: (T, T) => Boolean): List[T] = {
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      def merge(xs: List[T], ys: List[T]): List[T] = {
+        (xs, ys) match {
+          case (Nil, ys) => ys
+          case (xs, Nil) => xs
+          case (x :: xs1, y :: ys1) =>
+            if (lt(x, y)) x :: merge(xs1, ys)
+            else y :: merge(xs, ys1)
+        }
+      }
+
+      val (fst, snd) = xs splitAt n
+      merge(sort(fst)(lt), sort(snd)(lt))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums = List(2, 5, 23, 11, -2, 10)
+    println(sort(nums)((x: Int, y: Int) => x < y))
+
+    val books = List("Java", "C#", "C", "Angular")
+    println(sort(books)((x: String, y: String) => x.compareTo(y) < 0))
+  }
+}
