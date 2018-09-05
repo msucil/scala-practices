2c54b94c60a1ed19cdc98e559699dfb28995bcff
msucil <np.msushil@gmail.com>
2018-09-05 23:18:13 +0545

    Add Pair Of Prime

---
+++ src/main/scala/com/msucil/practice/scala/fp/collection/PairOfPrime.scala
@@ -0,0 +1,32 @@
+package com.msucil.practice.scala.fp.collection
+
+/**
+  * Given a positive integer N, find all pairs of positive integer i and j
+  * with 1 <= j < i < n such that i + j is prime
+  */
+object PairOfPrime {
+
+  def isPrime(n: Int): Boolean = (2 until n) forall( n % _ != 0)
+
+  def pairOfPrime1(n: Int): Unit = {
+    val primePairs = (1 until n) flatMap(i =>
+      1 until i map(j => (i, j))) filter (pair => isPrime(pair._1 + pair._2))
+    println(primePairs)
+  }
+
+  def pairOfPrime2(n: Int): Unit = {
+    val primePairs = for {
+      i <- 1 until n
+       j <- 1 until i
+      isPrime(i+j)
+    } yield (i, j)
+
+    println(primePairs)
+  }
+
+  def main(args: Array[String]): Unit = {
+    pairOfPrime1(7)
+    pairOfPrime2(7)
+  }
+
+}
